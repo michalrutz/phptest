79da31784febda75b7cb801245247c7358cb1eef
michalrutz <michal.rutz@gmail.com>
2020-08-21 22:33:27 +0200

    ajax

--- server.php
+++ server.php
@@ -1,10 +1,14 @@
 <?php
 var_dump($_FILES);
+$res = array();
+
 
 foreach ($_FILES["myFiles"]["tmp_name"] as $key => $value) {
 	$name = "uploads/" . basename($_FILES["myFiles"]["name"][$key]);
 	move_uploaded_file($value, $name);
 };
+$res["msg"] = "image uploaded successfully";
+
 
 // the message
 $msg = "First line of text\nSecond line of text";
@@ -15,4 +19,6 @@ $subject = "My subject";
 $to = "michal.rutz@gmail.com";
 
 // send email
-mail( $to , $subject , $msg);
+mail($to, $subject, $msg);
+
+json_encode($res);
